967df56eb66908ad27bc7db4a19445516074eb93
KK964 <morrkade05@gmail.com>
2020-06-14 12:41:44 -0500

    going insane rn

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ client.on('ready', () => {
     console.log('slimejamm is now ON');});
 client.on('message', msg => {
     if(msg.content === '!ssj on'){
-        if(!msg.member.cache.roles.has("295432346927104000")){msg.react('❌'); return;}
+        if(!msg.member.roles.cache.has("295432346927104000")){msg.react('❌'); return;}
         enabled = 1;
         console.log('slimejamm is now ON');
         msg.react('✅');}
     if(msg.content === 'ssj off'){
-        if(!msg.member.cache.roles.has("295432346927104000")){msg.react('❌'); return;}
+        if(!msg.member.roles.cache.has("295432346927104000")){msg.react('❌'); return;}
         enabled = 0;
         console.log('slimejamm is now OFF');
         msg.react('✅');}
-    if(enabled !== 1){return};
-    if(!msg.member.cache.roles.has("721622447756935208")){return};
+    if(!enabled == 1){return};
+    if(!msg.member.roles.cache.has("721622447756935208")){return};
     msg.react('707263484035072000');});
 client.login(process.env.token);
