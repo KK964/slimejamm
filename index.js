4a6c43a2899c4b27ce00d92bf6d35db73e415675
KK964 <morrkade05@gmail.com>
2020-06-14 10:23:53 -0500

    added on off toggle

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var enabled = 1;
 client.on('ready', () => {
     console.log(`Slimejamm is online`);
-});
+    enabled = 1;
+    console.log('slimejamm is now ON');});
 client.on('message', msg => {
+    if(msg.content === '!ssj on'){
+        if(!msg.member.roles.cache.has("295432346927104000")){msg.react('❌'); return;}
+        enabled = 1;
+        console.log('slimejamm is now ON');
+        msg.react('✅');}
+    if(msg.content === 'ssj off'){
+        if(!msg.member.roles.cache.has("295432346927104000")){msg.react('❌'); return;}
+        enabled = 0;
+        console.log('slimejamm is now OFF');
+        msg.react('✅');}
+    if(enabled !== 1){return};
     if(!msg.member.roles.cache.has("721622447756935208")){return};
-    msg.react('707263484035072000');
-});
+    msg.react('707263484035072000');});
 client.login(process.env.token);
