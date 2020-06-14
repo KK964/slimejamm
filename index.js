8068702b33cc988a64564db650b27023246c9d5a
KK964 <morrkade05@gmail.com>
2020-06-14 13:23:00 -0500

    added KK summon

--- index.js
+++ index.js
@@ -20,14 +20,14 @@ client.on('message', msg => {
     if(!msg.member.roles.cache.has("721622447756935208")){return};
     msg.react('707263484035072000');
     if(message.author.bot) return;
-    let msg = msg.content.toLocaleLowerCase();
+    let smsg = msg.content.toLocaleLowerCase();
     let sender= msg.member.displayName;
     let senderm = msg.members;
     let KK964 = client.users.cache.get('426892116258717707')
     var kk = ["kk"]
     for (x = 0; x < kk.length; x++) {
-        if (msg.includes(kk[x])){
-            KK964.send(`${sender} says ${msg}.`)
+        if (smsg.includes(kk[x])){
+            KK964.send(`${sender} says ${smsg}.`)
         }
     }
 });
