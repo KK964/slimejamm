6f45241ec11bc576618771f1245dde4fa001a380
KK964 <morrkade05@gmail.com>
2020-06-15 18:54:46 -0500

    changed member to author

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.on('message', msg => {
         enabled = 0;
         console.log('slimejamm is now OFF');
         msg.react('✅');}
-    if(msg.member = null){return};
+    if(msg.author = null){return};
     if(!enabled == 1){return};
     if(!msg.member.roles.cache.has("721622447756935208")){return};
     msg.react('707263484035072000');
