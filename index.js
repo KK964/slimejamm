f366f1e06fad851a75bab3de70223b5686a2489a
KK964 <morrkade05@gmail.com>
2020-06-24 16:25:46 -0500

    changed everything to work on private server

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+require("dotenv").config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
+require("dotenv-flow").config();
+const config = {
+    token: process.env.TOKEN
+};
 var enabled = 1;
 client.on('ready', () => {
     console.log(`Slimejamm is online`);
@@ -27,4 +32,4 @@ client.on('message', msg => {
     if(!msg.member.roles.cache.has("721622447756935208")){return};
     msg.react('707263484035072000');
 });
-client.login(process.env.token);
+client.login(config.token);
