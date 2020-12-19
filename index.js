07d6cb114059abada047c05f395c06cdb6063dc2
KK964 <morrkade05@gmail.com>
2020-12-19 12:52:22 -0600

    added ban db

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const wait = require('util').promisify(setTimeout);
 const fs = require('fs');
 require('dotenv-flow').config();
 const randEmoji = require('./randEmoji');
+const bans = require('./minecraft/banDB');
 const conf = require('./cfg.json');
 const ms = require('ms');
 const { send } = require('process');
 
 //Minecraft stuff
 const nameToUUID = require('./minecraft/nameUUID');
+const { isUndefined } = require('util');
 //
 
 //
@@ -21,16 +23,11 @@ const nameToUUID = require('./minecraft/nameUUID');
 
 const config = {
   token: process.env.TOKEN,
-  prefix: "!"
+  prefix: '!',
 };
 const queue = new Map();
 
-var sfx = [
-  'man-scream-01',
-  'fnaf',
-  'fart',
-  'yeahbb'
-]
+var sfx = ['man-scream-01', 'fnaf', 'fart', 'yeahbb'];
 
 //const guild = client.guilds.cache.get('295429838041382912')
 
@@ -41,7 +38,14 @@ client.on('ready', () => {
   console.log('SlimeSpider is now ON');
   wait(1000);
   //client.user.setPresence({activity:{name:"Sj's and berry's date", type:"STREAMING", url:"https://www.youtube.com/watch?v=dQw4w9WgXcQ"}, status:'online'});
-  client.user.setPresence({activity:{name:'is a slime', type:"STREAMING", url:"https://www.youtube.com/watch?v=dQw4w9WgXcQ"}, status:'online'});
+  client.user.setPresence({
+    activity: {
+      name: 'is a slime',
+      type: 'STREAMING',
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+    },
+    status: 'online',
+  });
   //const channel = console.log(client.guilds.cache.get('295429838041382912').channels.cache.get('713215253160263732'));
   /*client.guilds.cache
     .get('295429838041382912')
@@ -66,92 +70,131 @@ client.on('message', (msg) => {
     }
     msg.react('✅').then(msg.react('❌'));
   }
-  if(msg.content.startsWith(config.prefix)) {
-  switch(args[0]) {
-    case 'ssj': {
-      if (args[1] === 'on') {
-        if (!msg.member.hasPermission('MUTE_MEMBERS')) {
-          msg.react('❌');
-          return;
+  if (msg.content.startsWith(config.prefix)) {
+    switch (args[0]) {
+      case 'ssj': {
+        if (args[1] === 'on') {
+          if (!msg.member.hasPermission('MUTE_MEMBERS')) {
+            msg.react('❌');
+            return;
+          }
+          enabled = 1;
+          console.log('SlimeSpider is now ON');
+          msg.react('✅');
+          client.user
+            .setPresence({
+              activity: { name: 'shipping berry and sj' },
+              status: 'online',
+            })
+            .then(console.log)
+            .catch(console.error);
         }
-      enabled = 1;
-      console.log('SlimeSpider is now ON');
-      msg.react('✅');
-      client.user
-        .setPresence({
-          activity: { name: 'shipping berry and sj' },
-          status: 'online',
-        })
-        .then(console.log)
-        .catch(console.error);
-    }
-    if (args[1] === 'off') {
-      if (!msg.member.hasPermission('MUTE_MEMBERS')) {
-        msg.react('❌');
-        return;
-      }
-      enabled = 0;
-      console.log('SlimeSpider is now OFF');
-      msg.react('✅');
-      client.user
-        .setPresence({ activity: { name: 'nothing ;-;' }, status: 'idle' })
-        .then(console.log)
-        .catch(console.error);
-    }
-    if (args[1] === `results`) {
-      const rankEmbed = new Discord.MessageEmbed()
-        .setColor('#fcba03')
-        .setAuthor(client.user.username, client.user.avatarURL())
-        .setTitle('SJ Shipping Sim Results')
-        .addFields({
-          name: 'Results',
-          value:
-            `**1. ${getUsername('295483724429262848')}**: 9.8/10\n` +
-            `**2. ${getUsername('295279743480365066')}**: 9.0\n` +
-            `**3. ${getUsername('276543437241843713')}**: 8.7/10\n` +
-            `**4. null**: 8.2/10\n` +
-            `**5. ${getUsername('202873006773633024')}**: 7.1/10\n` +
-            `**6. ${getUsername('554128619635736601')}**: 5.2/10\n` +
-            `**7. ${getUsername('268365742636793856')}**: 3.1/10\n` +
-            `**8. ${getUsername('728442617498566666')}**: 2.2/10\n` +
-            `**9. ${getUsername('635699957080260632')}**: 1.0/10\n` +
-            `**10. ${getUsername('718860903793164450')}**: 0.2/10\n`,
-        });
-      msg.author.send(rankEmbed).catch(() => {
-        msg.react('❌');
-        return;
-      });
-      msg.react('756694775436148816');
-    }
+        if (args[1] === 'off') {
+          if (!msg.member.hasPermission('MUTE_MEMBERS')) {
+            msg.react('❌');
+            return;
+          }
+          enabled = 0;
+          console.log('SlimeSpider is now OFF');
+          msg.react('✅');
+          client.user
+            .setPresence({ activity: { name: 'nothing ;-;' }, status: 'idle' })
+            .then(console.log)
+            .catch(console.error);
+        }
+        if (args[1] === `results`) {
+          const rankEmbed = new Discord.MessageEmbed()
+            .setColor('#fcba03')
+            .setAuthor(client.user.username, client.user.avatarURL())
+            .setTitle('SJ Shipping Sim Results')
+            .addFields({
+              name: 'Results',
+              value:
+                `**1. ${getUsername('295483724429262848')}**: 9.8/10\n` +
+                `**2. ${getUsername('295279743480365066')}**: 9.0\n` +
+                `**3. ${getUsername('276543437241843713')}**: 8.7/10\n` +
+                `**4. null**: 8.2/10\n` +
+                `**5. ${getUsername('202873006773633024')}**: 7.1/10\n` +
+                `**6. ${getUsername('554128619635736601')}**: 5.2/10\n` +
+                `**7. ${getUsername('268365742636793856')}**: 3.1/10\n` +
+                `**8. ${getUsername('728442617498566666')}**: 2.2/10\n` +
+                `**9. ${getUsername('635699957080260632')}**: 1.0/10\n` +
+                `**10. ${getUsername('718860903793164450')}**: 0.2/10\n`,
+            });
+          msg.author.send(rankEmbed).catch(() => {
+            msg.react('❌');
+            return;
+          });
+          msg.react('756694775436148816');
+        }
 
-    if(args[1] === 'test') {
-      ratweewee(getRandomSfx(), msg.member, serverQueue);
+        if (args[1] === 'test') {
+          ratweewee(getRandomSfx(), msg.member, serverQueue);
+        }
+      }
+      case 'mc': {
+        if (args[1] === 'lookup') {
+          nameToUUID.data.getUUIDNames(args[2], msg);
+        }
+        if (args[1] === 'uuid') {
+          nameToUUID.data.getUUID(args[2], msg);
+        }
+        if (args[1] === 'ban') {
+          if (!msg.member.permissions.has('BAN_MEMBERS')) return msg.react('❌');
+          if (args[2] == undefined || args[3] == undefined || args[4] == undefined)
+            return msg.channel.send('> **!mc ban <server> <username> <time|p|perm> <reason>\n');
+          let server = args[2];
+          let username = args[3];
+          let time = args[4];
+          let reason = args.slice(0, 4).join();
+          bans.data.addBan(server, username, time, reason, msg);
+        }
+        if (args[1] === 'mute') {
+          if (!msg.member.permissions.has('BAN_MEMBERS')) return msg.react('❌');
+          if (args[2] == undefined || args[3] == undefined)
+            return msg.channel.send('> **!mc mute <username> <reason>\n');
+          let username = args[2];
+          let reason = args.slice(0, 2).join();
+          bans.data.addMute(username, reason, msg);
+        }
+        if (args[1] === 'bans') {
+          if (!msg.member.permissions.has('BAN_MEMBERS')) return msg.react('❌');
+          if (args[2] == undefined)
+            return msg.channel.send('> **!mc bans <username>** - get users bans');
+          let username = args[2];
+          bans.data.getBans(username, msg);
+        }
+        if (args[1] === 'mutes') {
+          if (!msg.member.permissions.has('BAN_MEMBERS')) return msg.react('❌');
+          if (args[2] == undefined)
+            return msg.channel.send('> **!mc mutes <username>** - get users mutes');
+          let username = args[2];
+          bans.data.getMutes(username, msg);
+        }
+        if (args[1] == undefined || args[1] === 'help') {
+          msg.channel.send(
+            '> **!mc lookup <username>** - Show name history\n' +
+              '> **!mc uuid <username>** - Show users uuid\n'
+          );
+          if (msg.member.permissions.has('BAN_MEMBERS')) {
+            msg.channel.send(
+              '> **!mc ban <server> <username> <time> <reason>** - add user to ban db\n' +
+                '> **!mc bans <username>** - get users bans' +
+                '> **!mc mute <username> <reason>** - add user to mute db\n' +
+                '> **!mc mutes <username>** - get users mutes\n'
+            );
+          }
+        } else if (args[2] == undefined) {
+          nameToUUID.data.getUUIDNames(args[1], msg);
+        }
+      }
     }
   }
-  case 'mc': {
-    if(args[1] === 'lookup') {
-      nameToUUID.data.getUUIDNames(args[2], msg);
-    }
-    if(args[1] === 'uuid') {
-      nameToUUID.data.getUUID(args[2], msg);
-    }
-    if(args[1] == undefined || args[1] === 'help') {
-      msg.channel.send('> **!mc lookup <username>** - Show name history\n' +
-      '> **!mc uuid <username>** - Show users uuid'
-      );
-    } else if(args[2] == undefined) {
-      nameToUUID.data.getUUIDNames(args[1], msg);
-    }
-  }
-}}
 
   if (msg.webhookID || msg.channel.type === 'dm' || msg.author == null || enabled != 1) return;
 
   if (msg.member.roles.cache.has('721622447756935208')) {
-    if (
-      msg.channel.id == '615023024260775946' ||
-      msg.channel.id == '459506938154057732'
-    ) {
+    if (msg.channel.id == '615023024260775946' || msg.channel.id == '459506938154057732') {
       return;
     }
     msg.react('707263484035072000');
@@ -159,13 +202,16 @@ client.on('message', (msg) => {
       msg.react(randEmoji.data.getEmoji());
     }
   }
-  if(msg.channel.id == '771040254466588723') {
+  if (msg.channel.id == '771040254466588723') {
     if (msg.author.bot) return;
     let msgChannel = arg[0];
-    const sendChannel = client.guilds.cache.get('295429838041382912').channels.cache.find(channel => channel.name === msgChannel);
+    const sendChannel = client.guilds.cache
+      .get('295429838041382912')
+      .channels.cache.find((channel) => channel.name === msgChannel);
     let msgText = arg.slice(1).join(' ');
-    if(!msgText || !sendChannel) return msg.react('❌');
-    if(staffCatagories.includes(sendChannel.parentID) && !msg.member.hasPermission('BAN_MEMBERS')) return msg.react('❌');
+    if (!msgText || !sendChannel) return msg.react('❌');
+    if (staffCatagories.includes(sendChannel.parentID) && !msg.member.hasPermission('BAN_MEMBERS'))
+      return msg.react('❌');
     //console.log(sendChannel);
     sendChannel.send(msgText);
     msg.react('✅');
@@ -176,21 +222,28 @@ client.on('message', (msg) => {
 client.on('voiceStateUpdate', (oldMember, newMember) => {
   const serverQueue = queue.get(client.guilds.cache.get('295429838041382912').id);
   let newUserChannel = newMember.member.voice.channel;
-  if(newMember.member.voice.channelID != '763857263798517761') {
-    if(getRandomInt(25) != 5) return;
+  if (newMember.member.voice.channelID != '763857263798517761') {
+    if (getRandomInt(25) != 5) return;
   }
-  if(newUserChannel) {
-    if((newMember.mute && !oldMember.mute) || (newMember.selfDeaf && !oldMember.selfDeaf) || (newMember.selfMute && !oldMember.selfMute) || 
-    (newMember.serverDeaf && !oldMember.serverDeaf) || (newMember.serverMute && !oldMember.serverMute) ||
-    
-    (!newMember.mute && oldMember.mute) || (!newMember.selfDeaf && oldMember.selfDeaf) || (!newMember.selfMute && oldMember.selfMute) || 
-    (!newMember.serverDeaf && oldMember.serverDeaf) || (!newMember.serverMute && oldMember.serverMute) ||
-
-    (newMember.streaming && !oldMember.streaming) || (!newMember.streaming && oldMember.streaming)
-    ) return;
+  if (newUserChannel) {
+    if (
+      (newMember.mute && !oldMember.mute) ||
+      (newMember.selfDeaf && !oldMember.selfDeaf) ||
+      (newMember.selfMute && !oldMember.selfMute) ||
+      (newMember.serverDeaf && !oldMember.serverDeaf) ||
+      (newMember.serverMute && !oldMember.serverMute) ||
+      (!newMember.mute && oldMember.mute) ||
+      (!newMember.selfDeaf && oldMember.selfDeaf) ||
+      (!newMember.selfMute && oldMember.selfMute) ||
+      (!newMember.serverDeaf && oldMember.serverDeaf) ||
+      (!newMember.serverMute && oldMember.serverMute) ||
+      (newMember.streaming && !oldMember.streaming) ||
+      (!newMember.streaming && oldMember.streaming)
+    )
+      return;
     ratweewee(getRandomSfx(), newMember.member, serverQueue);
   } else return;
-})
+});
 
 function getRandomSfx() {
   const randomsfx = Math.floor(Math.random() * sfx.length);
@@ -198,8 +251,7 @@ function getRandomSfx() {
 }
 
 function getUsername(id) {
-  return client.guilds.cache.get('295429838041382912').members.cache.get(id)
-    .displayName;
+  return client.guilds.cache.get('295429838041382912').members.cache.get(id).displayName;
 }
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
@@ -207,34 +259,34 @@ function getRandomInt(max) {
 
 async function ratweewee(arg, member, serverQueue) {
   const voiceChannel = member.voice.channel;
-  if(!voiceChannel) {
+  if (!voiceChannel) {
+    return;
+  }
+
+  if (!serverQueue) {
+    const queueContruct = {
+      voiceChannel: voiceChannel,
+      connection: null,
+      songs: [],
+      volume: 1,
+      playing: true,
+    };
+    queue.set('295429838041382912'.id, queueContruct);
+
+    queueContruct.songs.push(arg);
+
+    try {
+      var connection = await voiceChannel.join();
+      queueContruct.connection = connection;
+      play('295429838041382912', queueContruct.songs[0]);
+    } catch (err) {
+      console.log(err);
+      queue.delete('295429838041382912'.id);
       return;
-  } 
-
-  if(!serverQueue) {
-      const queueContruct = {
-          voiceChannel: voiceChannel,
-          connection: null,
-          songs: [],
-          volume: 1,
-          playing: true
-      };
-      queue.set('295429838041382912'.id, queueContruct);
-
-      queueContruct.songs.push(arg);
-
-      try {
-          var connection = await voiceChannel.join();
-          queueContruct.connection = connection;
-          play('295429838041382912', queueContruct.songs[0]);
-      } catch (err) {
-          console.log(err);
-          queue.delete('295429838041382912'.id);
-          return
-      }
+    }
   } else {
-      serverQueue.songs.push(arg);
-      return
+    serverQueue.songs.push(arg);
+    return;
   }
 }
 
@@ -248,16 +300,26 @@ function play(guild, song) {
 
   const dispatcher = serverQueue.connection
     .play(fs.createReadStream(`./audio/${song}.mp3`))
-    .on("finish", () => {
+    .on('finish', () => {
       serverQueue.songs.shift();
       play(guild, serverQueue.songs[0]);
     })
-    .on("error", error => console.error(error));
+    .on('error', (error) => console.error(error));
   dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
 }
 
 function uuidDash(uuid) {
-  return uuid.substr(0,8)+"-"+uuid.substr(8,4)+"-"+uuid.substr(12,4)+"-"+uuid.substr(16,4)+"-"+uuid.substr(20);
+  return (
+    uuid.substr(0, 8) +
+    '-' +
+    uuid.substr(8, 4) +
+    '-' +
+    uuid.substr(12, 4) +
+    '-' +
+    uuid.substr(16, 4) +
+    '-' +
+    uuid.substr(20)
+  );
 }
 
 var modules = {
@@ -269,8 +331,17 @@ var modules = {
   },
   returnNames: function (names, msg) {
     msg.channel.send(names);
-  }
-}
+  },
+  noEntries: function (name, msg) {
+    msg.channel.send(name + ' has no records in the database.');
+  },
+  wasError: function (err, msg) {
+    msg.channel.send('was error ;-;\n' + '`' + err + '`');
+  },
+  success: function (msg) {
+    msg.react('✅');
+  },
+};
 exports.data = modules;
 
 client.login(config.token);
--- minecraft/UUIDnames.js
+++ minecraft/UUIDnames.js
@@ -1,39 +1,43 @@
 const fetch = require('node-fetch');
-const index = require('../index');
+const { data } = require('../index');
 var methods = {
-    getNames: function (name, uuid, msg) {
-        var names = [];
-        fetch(`https://api.mojang.com/user/profiles/${uuid}/names`)
-        .then(response => {
-            const contentType = response.headers.get("content-type");
-            if(contentType && contentType.indexOf("application/json") !== -1) {
-                response.json().then(data => {
-                    for(var i = Object.keys(data).length; i >= 0 ; i--) {
-                        if(data[i]) {
-                            if(data[i].changedToAt != null && data[i].changedToAt != undefined && data[i].name != null && data[i].name != undefined) {
-                                names.push(`${data[i].name} | ${getDate(data[i].changedToAt)} days ago`);
-                            } else {
-                                if(data[i].name != null && data[i].name != undefined) {
-                                    names.push(data[i].name);
-                                }
-                            }
-                        }
-                    }
-                    if(!names.length == null) {
-                        names.push(name);
-                    }
-                    index.data.returnNames(names, msg);
-                })
-            } else {
-                index.data.unknownUser(name, msg);
+  getNames: function (name, uuid, msg) {
+    var names = [];
+    fetch(`https://api.mojang.com/user/profiles/${uuid}/names`).then((response) => {
+      const contentType = response.headers.get('content-type');
+      if (contentType && contentType.indexOf('application/json') !== -1) {
+        response.json().then((data) => {
+          for (var i = Object.keys(data).length; i >= 0; i--) {
+            if (data[i]) {
+              if (
+                data[i].changedToAt != null &&
+                data[i].changedToAt != undefined &&
+                data[i].name != null &&
+                data[i].name != undefined
+              ) {
+                names.push(`${data[i].name} | ${getDate(data[i].changedToAt)} days ago`);
+              } else {
+                if (data[i].name != null && data[i].name != undefined) {
+                  names.push(data[i].name);
+                }
+              }
             }
-        })
-    }
-}
+          }
+          if (!names.length == null) {
+            names.push(name);
+          }
+          data.returnNames(names, msg);
+        });
+      } else {
+        data.unknownUser(name, msg);
+      }
+    });
+  },
+};
 
-function getDate (timeInt) {
-    var time = Math.floor((Date.now() - timeInt)/1000/60/60/24);
-    return time;
+function getDate(timeInt) {
+  var time = Math.floor((Date.now() - timeInt) / 1000 / 60 / 60 / 24);
+  return time;
 }
 
 exports.data = methods;
